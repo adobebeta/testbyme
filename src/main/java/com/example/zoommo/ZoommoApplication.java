2636aa539e7e85c249ce5ebce2a0635a9459d3ce
Adobe Beta <adobebeta1997@gmail.com>
2019-03-11 04:19:14 +0700

    Initial commit

---
+++ src/main/java/com/example/zoommo/ZoommoApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoommo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoommoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoommoApplication.class, args);
+	}
+
+}
